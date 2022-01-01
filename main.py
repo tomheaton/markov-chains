733d452532fada93d6856e4cb7ca1407643c186b
Tom <thomas.m.heaton@gmail.com>
2022-01-01 16:18:46 +0100

    code

--- main.py
+++ main.py
@@ -5,7 +5,39 @@ import seaborn as sns
 
 
 def main() -> None:
-    print(f'hello world')
+    plt.style.use('ggplot')
+    plt.rcParams['font.family'] = 'sans-serif'
+    plt.rcParams['font.serif'] = 'Ubuntu'
+    plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['axes.labelsize'] = 12
+    plt.rcParams['axes.labelweight'] = 'bold'
+    plt.rcParams['axes.titlesize'] = 12
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
+    plt.rcParams['legend.fontsize'] = 12
+    plt.rcParams['figure.titlesize'] = 12
+    plt.rcParams['image.cmap'] = 'jet'
+    plt.rcParams['image.interpolation'] = 'none'
+    plt.rcParams['figure.figsize'] = (12, 10)
+    plt.rcParams['axes.grid'] = False
+    plt.rcParams['lines.linewidth'] = 2
+    plt.rcParams['lines.markersize'] = 8
+    colors = ['xkcd:pale orange', 'xkcd:sea blue', 'xkcd:pale red', 'xkcd:sage green', 'xkcd:terra cotta',
+              'xkcd:dull purple', 'xkcd:teal', 'xkcd: goldenrod', 'xkcd:cadet blue', 'xkcd:scarlet']
+
+    start = 0
+    x = []
+    n = 10000
+    for i in range(n):
+        step = np.random.choice([-1, 1], p=[0.5, 0.5])
+        start = start + step
+        x.append(start)
+
+    plt.plot(x)
+    plt.xlabel('Steps', fontsize=20)
+    plt.ylabel(r'$S_{n}$', fontsize=20)
+    plt.show()
 
 
 if __name__ == '__main__':
