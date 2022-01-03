72c454063f228068cefea3963bc0dacbc9e05b05
Tom <thomas.m.heaton@gmail.com>
2022-01-03 16:48:04 +0000

    work

--- modelling/main.py
+++ modelling/main.py
@@ -5,3 +5,5 @@ if __name__ == '__main__':
     print("Creating Markov Chain...")
 
     markov_chain = MarkovChain()
+    markov_chain.learn("I am Sam. Sam I am. I do not like green eggs and ham.")
+    markov_chain.display_memory()
--- modelling/markov_chain.py
+++ modelling/markov_chain.py
@@ -4,4 +4,18 @@ class MarkovChain:
     def __init__(self):
         self.memory = {}
 
-    
+    def learn_internal(self, key: any, value: any):
+        if key not in self.memory:
+            self.memory[key] = []
+
+        self.memory[key].append(value)
+
+    def learn(self, text: str):
+        # Tokenize the text.
+        tokens = text.split(" ")
+        x = [(tokens[i], tokens[i+1]) for i in range(0, len(tokens) - 1)]
+        for element in x:
+            self.learn_internal(element[0], element[1])
+
+    def display_memory(self):
+        print(f'{self.memory}')
