b01a5cc45727532287192776533142b9f530e1f6
Tom <thomas.m.heaton@gmail.com>
2022-01-03 16:55:34 +0000

    new things :gear:

--- modelling/markov_chain.py
+++ modelling/markov_chain.py
@@ -1,10 +1,12 @@
+import random
+
 
 class MarkovChain:
 
     def __init__(self):
         self.memory = {}
 
-    def learn_internal(self, key: any, value: any):
+    def learn_key(self, key: any, value: any):
         if key not in self.memory:
             self.memory[key] = []
 
@@ -13,9 +15,28 @@ class MarkovChain:
     def learn(self, text: str):
         # Tokenize the text.
         tokens = text.split(" ")
-        x = [(tokens[i], tokens[i+1]) for i in range(0, len(tokens) - 1)]
+        x = [(tokens[i], tokens[i + 1]) for i in range(0, len(tokens) - 1)]
         for element in x:
-            self.learn_internal(element[0], element[1])
+            self.learn_key(element[0], element[1])
+
+    def next(self, current_state):
+        next_possible = self.memory.get(current_state)
+
+        if not next_possible:
+            next_possible = self.memory.keys()
+
+        return random.sample(next_possible, 1)[0]
+
+    def babble(self, amount, state=''):
+        if not amount:
+            return state
+
+        next_word = self.next(state)
+
+        if not next_word:
+            return state
+
+        return state + ' ' + self.babble(amount - 1, next_word)
 
     def display_memory(self):
         print(f'{self.memory}')
